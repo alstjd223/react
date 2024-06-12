48e8f9f821d0708b0aad7d7f7e0039b8d13d21b7
“alstjd223” <2023011@bssm.hs.kr>
2024-06-12 23:17:59 +0900

    react

---
+++ src/components/three.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/three.css';
+
+const Three = () => {
+  return (
+    <div className="Three">
+      <h2>three.js 파일이ㅑ</h2>
+    </div>
+  );
+};
+
+export default Three;
---
+++ src/components/two.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/two.css';
+
+const Two = () => {
+  return (
+    <div className="Two">
+      <h2>two.js 파일이야!</h2>
+    </div>
+  );
+};
+
+export default Two;
